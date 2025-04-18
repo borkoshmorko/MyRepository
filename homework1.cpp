dba67ee22a34908acdbe809dc0c2c995c9a04748
borkoshmorko <bobigeshev@gmail.com>
2025-04-19 00:49:58 +0300

    250418Homework

---
+++ homework1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n, max = 0, secondMax = 0;
+	cout << "How many numbers do you want to enter?: ";cin >> n;
+	int* numbers = new int[n];
+	cout << "Enter your numbers: ";
+	for (int i = 0;i < n;i++) {
+		cin >> numbers[i];
+	}
+	for (int i = 0;i < n;i++) {
+		if (numbers[i] > max) {
+			max = numbers[i];
+		}
+	}
+	for (int i = 0;i < n;i++) {
+		if (numbers[i] < max && numbers[i] > secondMax) {
+			secondMax = numbers[i];
+		}
+	}
+	cout << "The second highest number is " << secondMax << ".";
+
+	delete[] numbers;
+}
